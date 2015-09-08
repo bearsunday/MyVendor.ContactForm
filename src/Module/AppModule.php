3fae56518352796254963d8006e12985da90ae5c
koriym <akihito.koriyama@gmail.com>
2015-09-08 10:30:54 +0900

    rename to AuraInputModule

--- src/Module/AppModule.php
+++ src/Module/AppModule.php
@@ -8,6 +8,7 @@ use MyVendor\ContactForm\Form\ContactForm;
 use MyVendor\ContactForm\Form\LoginForm;
 use MyVendor\ContactForm\Form\NameForm;
 use Ray\Di\AbstractModule;
+use Ray\WebFormModule\AuraInputModule;
 use Ray\WebFormModule\FormInterface;
 use Ray\WebFormModule\WebFormModule;
 
@@ -19,7 +20,7 @@ class AppModule extends AbstractModule
     protected function configure()
     {
         $this->install(new PackageModule);
-        $this->install(new WebFormModule);
+        $this->install(new AuraInputModule);
         $this->bind(FormInterface::class)->annotatedWith('name')->to(NameForm::class);
         $this->bind(FormInterface::class)->annotatedWith('contact_form')->to(ContactForm::class);
         $this->bind(FormInterface::class)->annotatedWith('login_form')->to(LoginForm::class);
