631090367ed1cfacaa6ca957f451bede9a6fbd4f
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-09-29 18:15:07 +0900

    add test

--- tests/Form/PreviewFormTest.php
+++ tests/Form/PreviewFormTest.php
@@ -2,9 +2,8 @@
 
 namespace MyVendor\ContactForm;
 
-use MyVendor\ContactForm\Form\NameForm;
 use MyVendor\ContactForm\Form\PreviewForm;
-use Ray\WebFormModule\AbstractForm;
+use MyVendor\ContactForm\Resource\Page\Preview;
 use Ray\WebFormModule\FormFactory;
 
 class PreviewFormTest extends \PHPUnit_Framework_TestCase
@@ -17,40 +16,19 @@ class PreviewFormTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         parent::setUp();
-        /* @var $form NameForm */
         $this->form = (new FormFactory())->newInstance(PreviewForm::class);
     }
 
     public function testValdationFailed()
     {
-        $isValid = $this->form->apply([]);
-        $this->assertFalse($isValid);
     }
 
-    public function testValidationSuccess()
+    public function testSet()
     {
-        $isValid = $this->form->apply([
-            'name' => 'BEAR',
-            'number' => '20'
-        ]);
-        $this->assertTrue($isValid);
-    }
-
-    public function testHtml()
-    {
-        $html = (string) $this->form;
-        $this->assertContains('<form method="post"', $html);
-        $this->assertContains('<input type="hidden" name="is_preview" value="1" />', $html);
-        $this->assertContains('<input id="name" type="text" name="name" size="20" maxlength="20"', $html);
-    }
-
-    public function testHiddenForm()
-    {
-        $html = $this->form->getHiddenForm();
-        $this->assertContains('<form method="post"', $html);
-        $this->assertContains('<input type="hidden" name="is_preview" value="0" />', $html);
-        $this->assertContains('<input type="hidden" name="name" />', $html);
-        $this->assertContains('<input type="hidden" name="number" value="10" />', $html);
-        $this->assertContains('<input type="submit" name="submit" value="Submit" />', $html);
+        $this->form->apply(['name' => ['0' => 'hello']]);
+        $ro = new Preview($this->form);
+        $ro['name[0]'] = 'hello';
+        $this->form->setValues($ro);
+        $this->assertSame('hello', $ro['name']['0']);
     }
 }
